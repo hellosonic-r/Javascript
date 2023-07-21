9ac205e4c91ff02f46cb440ec4786c447fc7900f
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-07-21 11:48:45 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -0,0 +1,10 @@
+function solution(str1, str2) {
+    var answer = 2;
+    for (var i = 0; i < str1.length-str2.length+1; i++){
+        if (str1.slice(i,i+str2.length) == str2){
+            answer = 1; 
+            break
+        }
+    }
+    return answer;
+}
