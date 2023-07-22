0e749111ab75df9a4afadeff3a9b666ed8d66fbd
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-07-22 12:17:16 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    var answer = 0;
+
+    for (var i = 0; i < 10; i++){
+        if (numbers.includes(i)) {
+            continue;
+        } else {
+            answer += i;
+        }
+    }
+    return answer;
+}
