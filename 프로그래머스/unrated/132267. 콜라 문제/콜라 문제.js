8ed570767a8ffdcc6b4efb1a510773b383f981c4
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-07-24 14:07:01 +0900

    [unrated] Title: 콜라 문제, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -0,0 +1,9 @@
+function solution(a, b, n) {
+    var answer = 0;
+    while (1){
+        if (n < a) break;
+        answer += (parseInt(n/a))*b; //받을 수 있는 콜라병
+        n = (parseInt(n/a)*b) + (n % a);
+    }
+    return answer;
+}
