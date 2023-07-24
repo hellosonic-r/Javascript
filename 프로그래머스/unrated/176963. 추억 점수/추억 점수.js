58e6bee6f6f562594c313f9a603b042232c3db6d
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-07-24 14:17:25 +0900

    [unrated] Title: 추억 점수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,19 @@
+function solution(name, yearning, photo) {
+    var answer = [];
+    var len = name.length; //4
+    var obj = {};
+    for (var i = 0; i < len; i++){
+        obj[name[i]] = yearning[i];
+    }
+    
+    for (var p of photo){
+        var temp = 0;
+        for (var k of p){
+            if (k in obj) {
+                temp += obj[k];
+            }
+        }
+        answer.push(temp);
+    }
+    return answer;
+}
