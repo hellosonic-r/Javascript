0bb76a781c1a8eb1d0e9f6999d953dabec71c3b1
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-07-24 12:40:33 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,13 @@
+function solution(food) {
+    var answer = '';
+    
+    for (var i = 1; i < food.length; i++){
+        answer += String(i).repeat(parseInt(food[i]/2))
+    }
+    answer += "0";
+    for (var i = food.length-1; i > 0; i--){
+        answer += String(i).repeat(parseInt(food[i]/2))
+
+    }
+    return answer;
+}
