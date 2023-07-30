224114c97b9acabadbcb4f58c115d37be4d05b24
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-07-30 11:53:32 +0900

    [unrated] Title: 카드 뭉치, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,22 @@
+function solution(cards1, cards2, goal) {
+    var answer = '';
+    var flag = true;
+    while (goal.length > 0){
+        var pick = goal.shift();
+        if (pick == cards1[0]) {
+            cards1.shift()
+        } else if (pick == cards2[0]) {
+            cards2.shift()
+        } else if ((pick != cards1[0]) && (pick != cards2[0])) {
+            flag = false;
+            break;
+        };
+        
+    }
+    if (flag == true) {
+        answer = "Yes";
+    } else {
+        answer = "No";
+    }
+    return answer;
+}
