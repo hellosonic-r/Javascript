dd3aafdfe2862b662819e05bbd0fe9f336c80812
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-07-30 15:50:40 +0900

    [level 1] Title: 실패율, Time: 4.84 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -0,0 +1,32 @@
+function solution(N, stages) {
+    var answer = [];
+    var obj = {};
+    var mo = stages.length;
+    
+    for (var i = 1; i < N+1; i++){
+        var cnt = 0;
+        for (var j of stages){
+            if (j == i) {
+                cnt += 1;
+            }
+        }
+        obj[i] = cnt/mo;
+        mo -= cnt;
+    }
+    
+    var result = []
+    
+    for (var s in obj){
+        result.push([Number(s), Number(obj[s])]);
+    }
+    result.sort(function(a,b){
+        return b[1] - a[1];
+    });
+    console.log(result);
+    
+    for (var k of result){
+        answer.push(k[0]);
+    }
+    
+    return answer;
+}
