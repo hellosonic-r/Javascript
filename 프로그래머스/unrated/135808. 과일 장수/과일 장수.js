dbad3bc548c1724b2b190dc276ac1ea1b9f8643c
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-07-30 12:21:34 +0900

    [unrated] Title: 과일 장수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,20 @@
+// 1점부터 k점까지 
+// 한 상자에 사과를 m개씩 포장
+// 사과 중 가장 낮은 점수가 p, 사과 한 상자의 가격은 p*m
+
+function solution(k, m, score) {
+    var answer = 0;
+    score.sort(function(a,b){
+        return b-a;
+    });
+    var result = [];
+    var time = parseInt(score.length / m);
+    
+    for (var i = 0; i < time; i++){ // 0,1,2,3
+        var temp = score.slice(m*i,m*i +m);
+        var min = Math.min(...temp);
+        answer += (min * m);
+    }
+    
+    return answer;
+}
