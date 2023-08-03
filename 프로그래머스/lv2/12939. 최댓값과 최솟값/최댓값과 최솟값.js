92d9460cc0dd61919df3b92abc25a3dd4d151c95
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-08-03 21:19:23 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    var answer = '';
+    var array = s.split(" ").map(Number);
+    answer += Math.min(...array);
+    answer += " ";
+    answer += Math.max(...array);
+    return answer;
+}
