0c1e3caf756f5a29528cbcad980834b26886744a
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-08-09 21:15:20 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,13 +1,14 @@
 function solution(d, budget) {
     var answer = 0;
-    d.sort(function(a,b){
-        return a-b;
-    })
-    for (var i of d){
-        if (i<=budget){
-            budget -= i;
-            answer += 1;
-        } else {
+    d.sort((a,b) => (a-b));
+    var total = 0;
+    for (var i = 0; i < d.length; i++){
+        total += d[i];
+        answer += 1;
+        if (total > budget) {
+            answer -= 1;
+            break;
+        } else if (total == budget) {
             break;
         }
     }
