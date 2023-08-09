307662b049174b34655d79bdf58952b228d6e4bc
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-08-09 18:10:04 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,9 +1,10 @@
 function solution(s) {
     var answer = '';
-    if (s.length % 2 == 0){
-        answer = s.slice(s.length/2 - 1, s.length/2 + 1);
-    } else {
-        answer = s[parseInt(s.length/2)];
+    if (s.length % 2 == 1){
+        answer = s[parseInt(s.length / 2)];
+    }
+    else {
+        answer = s.split("").slice(parseInt(s.length/2)-1, parseInt(s.length/2)+1).join("");
     }
     return answer;
 }
