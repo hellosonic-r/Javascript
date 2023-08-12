f5d261688a79dc6f5ac5fb37cae01dce39464c68
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-08-12 09:20:26 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,12 +1,15 @@
-function getGcd(a,b){
+function getGcd(a,b) {
     if (b == 0) return a;
     return getGcd(b, a%b);
 }
 function solution(n, m) {
     var answer = [];
-    var gcd = getGcd(n,m);
-    answer.push(gcd);
+    var arr = [n,m];
+    arr.sort((a,b) => a-b);
+    var gcd = getGcd(arr[0], arr[1]);
     var lcm = n*m / gcd;
+    answer.push(gcd);
     answer.push(lcm);
+    
     return answer;
 }
