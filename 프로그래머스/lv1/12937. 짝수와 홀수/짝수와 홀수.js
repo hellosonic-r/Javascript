cfea519a27b0a34a3204309fdd5216db882cae6d
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-09-20 23:20:28 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,9 +1,6 @@
 function solution(num) {
     var answer = '';
-    if (num % 2 == 0) {
-        answer = 'Even';
-    } else {
-        answer = 'Odd';
-    }
+    if (num%2==0) answer = "Even";
+    else answer = "Odd";
     return answer;
 }
