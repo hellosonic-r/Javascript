98dff71f1536fd905a1ce440122388abcb65de54
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-09-20 23:27:18 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,9 +1,9 @@
 function solution(n)
 {
     var arr = String(n).split("").map(Number);
-    var answer = arr.reduce(function(sum, currValue){
-       return sum + currValue; 
-    });
+    var answer = arr.reduce(function(tempSum, currValue){
+        return tempSum + currValue;
+    }, 0);
+
     return answer;
-    
 }
