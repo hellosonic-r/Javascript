646f312d2ed62cd5000aa7c67d06c17fa3d74742
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-09-22 17:04:00 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,13 +1,10 @@
 function solution(num) {
     var answer = 0;
     while (num != 1){
-        if (num % 2 == 0) {
-            num = num / 2;
-        } else {
-            num = (num * 3) + 1;
-        }
-        answer += 1;
-        if (answer == 501){
+        if (num % 2 == 0) num /= 2;
+        else num = (num * 3) + 1;
+        answer+=1;
+        if (answer == 501) {
             answer = -1;
             break;
         }
