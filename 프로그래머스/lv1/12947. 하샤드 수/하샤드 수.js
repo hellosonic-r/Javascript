7dcde5cf128c0bfe0cce1c78d889d58852e985e0
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-09-22 16:59:48 +0900

    [level 1] Title: 하샤드 수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,11 +1,4 @@
 function solution(x) {
-    var answer = true;
-    var sum = String(x).split("").map(Number).reduce(function(s, currValue){
-        return s + currValue;
-    });
-    
-    if (x % sum == 0) answer = true;
-    else answer = false;
-    
-    return answer;
+    const sum = String(x).split("").map(Number).reduce((tempSum, currValue) => {return tempSum + currValue});
+    return x % sum == 0;
 }
