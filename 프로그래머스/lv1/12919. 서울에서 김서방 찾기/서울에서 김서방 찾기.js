7e479771b81a7d2d94e10c98b510f6c0723a4ebc
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-09-23 23:30:14 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,7 +1,3 @@
 function solution(seoul) {
-    var answer = '';
-    var pos = seoul.indexOf('Kim');
-    answer = `김서방은 ${pos}에 있다`
-    return answer;
-    
+    return `김서방은 ${seoul.indexOf('Kim')}에 있다`
 }
