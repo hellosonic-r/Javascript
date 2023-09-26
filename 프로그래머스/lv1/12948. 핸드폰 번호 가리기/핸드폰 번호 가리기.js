0817c7a0ac43d1fb798b8c127ac1a78d2d1e6b4d
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-09-26 23:46:40 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,9 @@
+function solution(phone_number) {
+    let answer = ""
+    for (let i = 0; i < phone_number.length; i++){
+        if (i > phone_number.length-5) answer += phone_number[i];
+        else answer += "*";
+    }
+    return answer;
+
+}
