d3e21f161f9034e649b9b3909aae9a4e6e44f288
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-09-29 21:57:37 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,5 +1,6 @@
 function solution(n) {
     // return Number(String(n).split("").map(Number).sort((a,b) => (b-a)).join(""));
-    return Number(String(n).split("").map(Number).sort((a,b)=>(b-a)).join(""));
+    // return Number(String(n).split("").map(Number).sort((a,b)=>(b-a)).join(""));
+    return Number([...String(n)].map(Number).sort((a,b) => b-a).join(""));
     
 }
