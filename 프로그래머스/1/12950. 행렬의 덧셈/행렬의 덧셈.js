f0e156705cf63c54035ace5d96e2f48206f378da
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-10-26 18:58:52 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.38 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,15 @@
+function solution(arr1, arr2) {
+    var answer = [];
+    var y = arr1.length;
+    var x = arr1[0].length;
+    
+    for (var i = 0; i < y; i++){
+        var temp = [];
+        for (var j = 0; j < x; j++){
+            temp.push(arr1[i][j] + arr2[i][j]); 
+        }
+        answer.push(temp);
+    }
+    return answer;
+    
+}
