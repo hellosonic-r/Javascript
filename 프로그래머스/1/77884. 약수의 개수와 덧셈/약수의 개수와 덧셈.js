9e52772410c565132bb20ebf4c37c22962958b27
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-11-09 13:09:17 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.32 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,13 @@
+function solution(left, right) {
+    var answer = 0;
+    for (let num = left; num < right+1; num++){
+        var cnt = 0;
+        for (let i = 1; i < num+1; i++){
+            if (num % i == 0) cnt += 1;
+        }
+        if (cnt % 2 == 0) answer += num;
+        else answer -= num;
+    }
+    
+    return answer;
+}
