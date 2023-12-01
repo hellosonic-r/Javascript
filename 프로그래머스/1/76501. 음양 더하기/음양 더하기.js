3f5014771562e3a3e1c5f237741700baa63195a8
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-12-01 23:51:57 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,6 +1,6 @@
 function solution(absolutes, signs) {
     let answer = 0;
     absolutes.forEach((element, idx) => { signs[idx] ? answer += element : answer -= element });
-    
+
     return answer;
 }
