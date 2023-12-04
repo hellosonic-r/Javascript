703be40fd8e640c20c832f62c0a97ce9059400a4
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-12-04 23:38:06 +0900

    [level 1] Title: 약수의 합, Time: 0.43 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -3,4 +3,5 @@ function solution(n) {
     // let result = 0;
     // arr.forEach((element, idx) => {n%element==0 ? result += element : result += 0});
     return arr.reduce((s, currValue) => { return n%currValue == 0 ? s + currValue : s + 0},0);
+    
 }
