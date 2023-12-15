12ba70acdc6a3ca434bbbcffacbb3730d884aa19
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2023-12-15 18:36:10 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -7,8 +7,10 @@ function solution(price, money, count) {
         if (i>count) break;
         total += price * i;
     }
+    
     answer = total - money;
     if (answer < 0) answer = 0;
 
+    
     return answer;
 }
