0804726508ee28c713f0fb41192d5df82471b531
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2025-12-26 15:24:34 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/README.md
+++ 프로그래머스/1/12943. 콜라츠 추측/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라츠 추측 - 12943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.13 ms
+메모리: 33.5 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 2일 23:40:12
+2025년 12월 26일 15:24:33
 
 ### 문제 설명
 
--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,9 +1,14 @@
 function solution(num) {
-    let answer = 0;
-    while (num != 1 && answer != 500) {
-        num%2 == 0 ? num = num / 2 : num = (num * 3) + 1;
-        answer += 1;
+    let cnt = 0;
+    if (num === 1) return 0;
+    while (cnt < 500) {
+        if (num % 2 === 0) {
+            num = num / 2
+        } else {
+            num = num * 3 + 1;
+        }
+        cnt += 1;
+        if (num === 1) break;
     }
-    
-    return num == 1 ? answer : -1;
+    return cnt === 500 ? -1 : cnt;
 }
