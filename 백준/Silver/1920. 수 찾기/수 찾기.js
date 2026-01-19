8484b6b8f5a2662d813fde1f2a6fd6d239c1ba90
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2026-01-19 21:06:49 +0900

    [Silver IV] Title: 수 찾기, Time: 304 ms, Memory: 41232 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -8,32 +8,24 @@ const a = input[1]
   .sort((a, b) => a - b);
 const m = Number(input[2]);
 const arr = input[3].split(' ').map(Number);
-
-// console.log(n);
-// console.log(a);
-// console.log(m);
-// console.log(arr);
-
 const result = [];
 
 arr.forEach((num) => {
-  let start = 0;
-  let end = n - 1;
-  let isNumberInArray = false;
-
-  while (start <= end) {
-    let mid = parseInt((start + end) / 2);
-    if (num < a[mid]) {
-      end = mid - 1;
-    } else if (num > a[mid]) {
-      start = mid + 1;
-    } else {
-      isNumberInArray = true;
-      break;
-    }
-  }
-  if (isNumberInArray) result.push(1);
-  else result.push(0);
+  result.push(binarySearch(a, num));
 });
+
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) return 1;
+    if (arr[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+  return 0;
+}
 
 console.log(result.join('\n'));
