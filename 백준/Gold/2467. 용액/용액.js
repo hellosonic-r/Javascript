05fbd0ba77aaa15a60f61f14dbacc21b30c04fe1
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2026-01-28 17:06:02 +0900

    [Gold V] Title: 용액, Time: 192 ms, Memory: 21408 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.js
+++ 백준/Gold/2467. 용액/용액.js
@@ -3,34 +3,25 @@ let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 const n = Number(input[0]);
 const arr = input[1].split(" ").map(Number);
 
-let min = Infinity
+let min = Infinity;
 let resultLeft;
 let resultRight;
 
-for (let i = 0; i < n - 1; i++) {
-  binarySearch(arr, i, arr[i]);
-}
+let left = 0;
+let right = n - 1;
 
-resultLeft < resultRight
-  ? console.log(resultLeft, resultRight)
-  : console.log(resultRight, resultLeft);
-
-function binarySearch(arr, index, currentNum) {
-  let left = index + 1;
-  let right = arr.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    const sum = Math.abs(arr[mid] + currentNum);
-    if (min > sum) {
-      min = sum;
-      resultLeft = arr[mid];
-      resultRight = currentNum;
-    }
-
-    if (arr[mid] + currentNum >= 0) {
-      right = mid - 1;
-    } else {
-      left = mid + 1;
-    }
+while (left < right) {
+  const sum = Math.abs(arr[left] + arr[right]);
+  if (min > sum) {
+    min = sum;
+    resultLeft = arr[left];
+    resultRight = arr[right];
+  }
+  if (arr[left] + arr[right] >= 0) {
+    right--;
+  } else {
+    left++;
   }
 }
+
+console.log(resultLeft, resultRight);
