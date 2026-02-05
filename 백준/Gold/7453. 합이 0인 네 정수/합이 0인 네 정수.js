9362bd6b8de6a33ab7cd05118e7108edabab627c
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2026-02-05 12:43:07 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 5768 ms, Memory: 930512 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.js
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.js
@@ -6,7 +6,6 @@ const arrA = [];
 const arrB = [];
 const arrC = [];
 const arrD = [];
-
 for (let i = 1; i < n + 1; i++) {
   const arr = input[i].split(" ").map(Number);
   arrA.push(arr[0]);
@@ -15,23 +14,20 @@ for (let i = 1; i < n + 1; i++) {
   arrD.push(arr[3]);
 }
 
-/* 🔹 1. C + D 합을 Map에 저장 */
-const cdMap = new Map();
-
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < n; j++) {
-    const sum = arrC[i] + arrD[j];
-    cdMap.set(sum, (cdMap.get(sum) || 0) + 1);
-  }
-}
-
-/* 🔹 2. A + B를 돌면서 -(A+B) 조회 */
 let answer = 0;
+const map = new Map();
 
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < n; j++) {
     const sum = arrA[i] + arrB[j];
-    answer += cdMap.get(-sum) || 0;
+    map.set(sum, map.get(sum) + 1 || 1);
+  }
+}
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    const sum = arrC[i] + arrD[j];
+    answer += map.get(-sum) || 0;
   }
 }
 
