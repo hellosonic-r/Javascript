239076d7a7cf3af90a45019211d2b4c16a3df9ea
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2026-02-09 13:34:23 +0900

    [Gold IV] Title: 부분합, Time: 168 ms, Memory: 17012 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.js
+++ 백준/Gold/1806. 부분합/부분합.js
@@ -4,16 +4,16 @@ let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 const [n, s] = input[0].split(" ").map(Number);
 const arr = input[1].split(" ").map(Number);
 
+let answer = Infinity;
+// 5 1 3 5 10 7 4 9 2 8
+let initSum = 0;
 let left = 0;
 let sum = 0;
-let answer = Infinity;
-
-for (let right = 0; right < n; right++) {
-  sum += arr[right];
-
+for (let i = 0; i < n; i++) {
+  sum += arr[i];
   while (sum >= s) {
-    answer = Math.min(answer, right - left + 1);
     sum -= arr[left];
+    answer = Math.min(answer, i - left + 1);
     left++;
   }
 }
