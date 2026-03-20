dfb47a5916607f3b3970eed46626eb5e3888a11f
hwang minho <124658938+hellosonic-r@users.noreply.github.com>
2026-03-21 01:26:57 +0900

    [Silver I] Title: 그림, Time: 256 ms, Memory: 23648 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.js
+++ 백준/Silver/1926. 그림/그림.js
@@ -1,69 +1,56 @@
-//한 줄 입력
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-// let fs = require('fs');
-// let input = fs.readFileSync('/dev/stdin').toString().split(' ');
-
-// let num = Number(input);
-
-// for (let i = 1; i <= num; i++) {
-//   console.log(i);
-// }
-
-// 여러 줄 입력
-
-let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-
-const [n, m] = input[0].split(" ").map(Number);
+const [n, m] = input[0].split(' ').map(Number);
 const board = [];
 
-for (let i = 1; i <= n; i++) {
-  board.push(input[i].split(" ").map(Number));
+for (let i = 1; i < n + 1; i++) {
+  board.push(input[i].split(' ').map(Number));
 }
+
+const visited = Array.from({ length: n }, () =>
+  Array.from({ length: m }, () => 0),
+);
 
 const dx = [0, 1, 0, -1];
 const dy = [-1, 0, 1, 0];
 
-const visited = Array.from({ length: n }, () => Array(m).fill(0));
-let count = 0;
-let maxArea = 0;
-
-const bfs = (sx, sy) => {
-  const queue = [];
-  queue.push([sx, sy]);
-  visited[sy][sx] = 1;
-  let area = 1;
-
-  while (queue.length !== 0) {
-    const [x, y] = queue.shift();
-
-    for (let i = 0; i < 4; i++) {
-      const nx = x + dx[i];
-      const ny = y + dy[i];
-
-      if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
-      else {
-        if ((visited[ny][nx] === 0) & (board[ny][nx] === 1)) {
-          queue.push([nx, ny]);
-          visited[ny][nx] = 1;
-          area++;
-        }
-      }
-    }
-  }
-  if (area > maxArea) {
-    maxArea = area;
-  }
-};
+let cnt = 0;
+let max = 0;
 
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < m; j++) {
     if (board[i][j] === 1 && visited[i][j] === 0) {
-      bfs(j, i);
-      count++;
+      cnt += 1;
+      max = Math.max(max, bfs(j, i));
     }
   }
 }
 
-console.log(count);
-console.log(maxArea);
+console.log(cnt);
+console.log(max);
+
+function bfs(sx, sy) {
+  let area = 1;
+  const queue = [];
+  queue.push([sx, sy]);
+  visited[sy][sx] = 1;
+
+  while (queue.length > 0) {
+    const [x, y] = queue.shift();
+    for (let i = 0; i < 4; i++) {
+      const nx = x + dx[i];
+      const ny = y + dy[i];
+
+      if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+      else {
+        if (board[ny][nx] === 1 && visited[ny][nx] === 0) {
+          queue.push([nx, ny]);
+          visited[ny][nx] = visited[y][x] + 1;
+          area += 1;
+        }
+      }
+    }
+  }
+  return area;
+}
